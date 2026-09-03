2a4ac8c8d16ca61e9e74f7b026355257102068aa
Sumit Kumar Chaurasiya <58983442+Sumit572-rgj@users.noreply.github.com>
2026-09-03 06:46:59 +0530

    Time: 5 ms (83.33%) | Memory: 122 MB (18.12%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int min = Integer.MAX_VALUE;
+        boolean allEven = true;
+
+        for (int num : nums1) {
+            min = Math.min(min, num);
+
+            if (num % 2 != 0) {
+                allEven = false;
+            }
+        }
+        if (allEven) {
+            return true;
+        }
+
+
+        return min % 2 != 0;
+    }
+}
