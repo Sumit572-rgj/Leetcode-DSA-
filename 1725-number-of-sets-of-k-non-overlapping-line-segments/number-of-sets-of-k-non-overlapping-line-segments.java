ddbe35e0c701b4e0f96e238fb51ec49a49019471
Sumit Kumar Chaurasiya <58983442+Sumit572-rgj@users.noreply.github.com>
2026-09-16 09:47:05 +0530

    Time: 26 ms (75.48%) | Memory: 62.9 MB (70.32%) - LeetSync

---
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numberOfSets(int n, int k) {
+        long MOD = 1000000007;
+
+        long[][] dp = new long[k + 1][n];
+
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+
+        for (int s = 1; s <= k; s++) {
+            long sum = 0;
+
+            for (int i = 1; i < n; i++) {
+                sum = (sum + dp[s - 1][i - 1]) % MOD;
+                dp[s][i] = (dp[s][i - 1] + sum) % MOD;
+            }
+        }
+
+        return (int) dp[k][n - 1];
+    }
+}
